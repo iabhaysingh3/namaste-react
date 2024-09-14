6d08ab83ec3dd70636ccc11cd7b77311c2ad6ffc
Abhay Singh <iabhaysingh3@gmail.com>
2024-09-14 23:20:57 +0530

    namaste react course

---
+++ App.js
@@ -0,0 +1,20 @@
+const heading = React.createElement("h1", {
+    id:"title"
+}, "Heading 1");
+
+const heading2 = React.createElement("h2", {
+    id:"title"
+}, "Heading 2");
+
+const container = React.createElement("div", {
+    id: "container"
+}, [heading, heading2]);
+
+console.log(heading);
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// passing a react element indside the root
+root.render(container);
