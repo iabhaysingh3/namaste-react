7eed2e66851258e28a8809c5f8dddb87e497c35e
Abhay Singh <iabhaysingh3@gmail.com>
2024-09-19 13:04:00 +0530

    namaste-react L2

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
+/*
+*
+* HRM - Hot Module Replacement
+* File Watcher algorithm - C++
+* BUNDLING
+* MINIFY
+* Cleaning our Code
+* Dev and Production Build
+* Super Fast build algorithm
+* Image Optimization
+* Caching while development
+* Compression
+* Compatable with older version of browsers
+* HTTPS on dev
+* Port Number
+* Consistent Hashing algorithm
+* Zero Config
+* Tree Shaking - Removing un-wanted code
+* 
+* Transitive Dependencies
+* 
+*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", {
     id:"title"
-}, "Heading 1");
+}, "Heading 1 from parcel");
 
 const heading2 = React.createElement("h2", {
     id:"title"
 }, "Heading 2");
 
 const container = React.createElement("div", {
-    id: "container"
+    id: "container",
+    hello:"world"
 }, [heading, heading2]);
 
 console.log(heading);
